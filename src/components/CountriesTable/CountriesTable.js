3ec4bdeb14288dd908ff94ccb876d14caa5d2ba5
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-22 18:25:42 +0900

    검색 input 넣기

---
+++ src/components/CountriesTable/CountriesTable.js
@@ -0,0 +1,5 @@
+const CountriesTable = () => {
+  return <div>컨트리테이블</div>;
+};
+
+export default CountriesTable;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,26 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 
-export default function Home() {
-  return <Layout>main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return (
+    <Layout>
+      <div>총 {countries.length}개의 나라를 검색했어요</div>
+      <SearchInput placeholder="나라를 입력하세요" />
+      <CountriesTable />
+    </Layout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/all`);
+  const countries = await res.json();
+  return {
+    props: {
+      countries,
+    },
+  };
+};
