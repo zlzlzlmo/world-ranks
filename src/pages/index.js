eaaa4bd149375ca9d677defe59aed03ae15a087a
hoonVelop-shin <78241389+hoonVelop-shin@users.noreply.github.com>
2021-08-22 18:01:22 +0900

    폴더 구조 잡기

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout/Layout";
+
+export default function Home() {
+  return <Layout>main</Layout>;
+}
